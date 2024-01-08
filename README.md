5375c09bcef562e0cc30a03f2c33b3af89ff635c
Daniel Goldenholz <dgoldenholz@users.noreply.github.com>
2024-01-07 19:18:53 -0500

    updates on bigben and readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # bigben
+
+There are several old bash script version that are called clock2.sh, clock3.sh clock4.sh and clock5.sh. There is also bigben_v2.py and bigben_old.py.
+
+The current version is bigben.py.
+
+It is assumed you will run this on linux. If not, feel free to modify.
+The sound files are assumed to be .wav files, which must be converted from the .m4a files. This is becuase on some linux systems it is hard to get a good codec for those. Just find a free converter and convert *.m4a to *.wav.
+
+In linux you will want this bigben.py to run every quarter hour. The following way is good in case your linux system ever shuts down, it will keep working next startup:
+
+how to add to startup
+crontab -e
+*/15 * * * * XDG_RUNTIME_DIR=/run/user/$(id -u) python ~/bigben/bigben.py
---
+++ bigben_old.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+# Will play grandfather clcok sounds
+# Version 1.4
+# Daniel Goldenholz
+
+# Library calls
+from datetime import datetime
+from time import sleep
+import simpleaudio as sa
+
+
+# MAIN CODE
+
+# constants
+# smallTIME is how many secs to wait
+smallTIME = 1
+last_check = datetime.now()
+
+# Initialize sound
+clocklist = ['01','02','03','04','05','06','07','08','09','10','11','12']
+quarterlist = ['quarter','half','3quarter']
+ticname = 'tic2.wav'
+wobj=[]
+print('loading hours...')
+for counter in range(12):
+    print(counter+1)
+    wobj.append(sa.WaveObject.from_wave_file(clocklist[counter] + '.wav'))
+
+# load the quarter hours
+qwobj=[]
+print('loading quarters...')
+for counter in range(3):
+    print(counter+1)
+    qwobj.append(sa.WaveObject.from_wave_file(quarterlist[counter] + '.wav'))
+
+print('loading tic.')
+twobj=sa.WaveObject.from_wave_file(ticname)
+
+print('Loaded.')
+
+dotic = 0
+while 1:
+    now = datetime.now()
+    delta_t = now - last_check
+    delta_t_sec = delta_t.total_seconds()
+    current_HR = now.strftime("%H")
+    current_T = now.strftime("%H%M")
+
+    oktoplay = (now.hour>=8) and (now.hour<23)
+    if delta_t_sec > 900 and oktoplay==1:
+        if now.minute==0:
+            H = now.hour % 12
+            if H==0:
+                H=12
+            play_obj = wobj[H-1].play()
+            play_obj.wait_done()
+            dotic=0
+            print('%s - %d' % (current_T,H))
+            last_check = now
+
+        if now.minute==15:
+            play_obj = qwobj[0].play()
+            play_obj.wait_done()
+            dotic=0
+            print('%s - quarter' % current_T)
+            last_check = now
+
+        if now.minute==30:
+            play_obj = qwobj[1].play()
+            play_obj.wait_done()
+            dotic=0
+            print('%s - half' % current_T)
+            last_check = now
+
+        if now.minute==45:
+            play_obj = qwobj[2].play()
+            play_obj.wait_done()
+            dotic=0
+            print('%s - 3quarter' % current_T)
+            last_check = now
+
+    sleep(smallTIME)
